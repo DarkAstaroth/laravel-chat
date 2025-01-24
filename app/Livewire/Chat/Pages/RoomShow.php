289f47534b034e38632a8387e35b08069e0da01b
Victor Manuel Ninahuanca Ayala <victor.ninahuanca@agetic.gob.bo>
2025-01-24 16:32:26 -0400

    feat: commit inicial

---
+++ app/Livewire/Chat/Pages/RoomShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Chat\Pages;
+
+use Livewire\Component;
+
+class RoomShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.chat.pages.room-show');
+    }
+}
