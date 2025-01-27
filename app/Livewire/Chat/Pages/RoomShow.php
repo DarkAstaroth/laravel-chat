f9f4bc684bf41a469c020d4a3acbcad7d3ef8cad
Victor Manuel Ninahuanca Ayala <victor.ninahuanca@agetic.gob.bo>
2025-01-27 16:25:45 -0400

    feat: crear mensajes para la sala de chat

--- app/Livewire/Chat/Pages/RoomShow.php
+++ app/Livewire/Chat/Pages/RoomShow.php
@@ -2,17 +2,29 @@
 
 namespace App\Livewire\Chat\Pages;
 
+use App\Models\Message;
 use App\Models\Room;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class RoomShow extends Component
 {
     public Room $room;
+    public string $body;
+
 
     #[Layout('layouts.app')]
     public function render()
     {
         return view('livewire.chat.pages.room-show');
+    }
+
+    #[Rule('required')]
+    public function submit()
+    {
+        $this->validate();
+        $message = Message::make($this->only('body'));
+        // $message->
     }
 }
--- resources/views/livewire/chat/pages/room-show.blade.php
+++ resources/views/livewire/chat/pages/room-show.blade.php
@@ -14,8 +14,17 @@
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg col-span-9">
                 <div class="p-6 text-gray-900">
-                    Chat
+                    Messages
                 </div>
+
+                <form class="mt-3 p-4">
+                    <label for="body" class="sr-only">Message</label>
+                    <textarea name="body" id="body" rows="4"
+                        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+                        placeholder="Say something" wire:model="body" x-on:keydown.shift="shift = true" x-on:keyup.shift="shift = false"
+                        x-on:keydown.enter="if (!shift || !$event.target.value) { $event.preventDefault() }"
+                        x-on:keyup.enter.prevent="if (!shift && $event.target.value) { $wire.submit(); handleTypingFinished() }"></textarea>
+                </form>
             </div>
         </div>
     </div>
